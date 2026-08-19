0497931edd1d81476cf36e5047aabf8c5d139e4b
Thawan Silva <twnpsilva@gmail.com>
2026-08-19 16:54:38 -0300

    wip: initialize file config class

---
+++ inc/config/FileConfig.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FileConfig.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thaperei <thaperei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/19 15:26:35 by thaperei          #+#    #+#             */
+/*   Updated: 2026/08/19 16:49:29 by thaperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WEBSERV_CONFIG_FILECONFIG_HPP
+#define WEBSERV_CONFIG_FILECONFIG_HPP
+
+class FileConfig
+{
+	std::string _path;
+
+  public:
+	FileConfig();
+	FileConfig(std::string const path);
+	~FileConfig();
+
+	std::string getPath() const;
+	std::string readFile();
+	bool isReadable() const;
+	bool isFileExists() const;
+}
+
+#endif
